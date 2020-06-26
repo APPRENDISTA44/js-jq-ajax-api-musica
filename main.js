8ed4e9772eab6b3a38185fdaf85b02b5355e4230
Luca Isidoro <apprendista44@gmail.com>
2020-06-26 18:04:57 +0200

    esercizio con bonus completo

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
       url:'https://flynn.boolean.careers/exercises/api/array/music',
       method: 'GET',
       success: function (data) {
+        console.log(data);
         console.log(data.response);
         var cds = print(data.response)
       }
@@ -16,8 +17,21 @@ $(document).ready(function() {
     for (var i = 0; i < arrayCds.length; i++) {
       var html = template(arrayCds[i]);
       $('.cds-container').append(html);
-
     }
-
   }
+
+  $('select').change( function () {
+    var value = $(this).val();
+    console.log(value);
+    $('.cd').each(function(){
+      $(this).show();
+      var genre = $(this).children('.genre').text();
+      if (value == 'all') {
+        $(this).show();
+      }else if (value != genre) {
+        $(this).hide();
+      }
+    })
+
+  })
 });
